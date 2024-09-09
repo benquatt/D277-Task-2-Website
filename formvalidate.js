a3f1362aafffd1d209af88a715c307cb1573bbf2
benquatt <bquattl@wgu.edu>
2024-09-09 16:25:17 -0400

    Separated script to own file

---
+++ formvalidate.js
@@ -0,0 +1,9 @@
+function validateEmail() {
+    let emailField1 = document.getElementById("email1").value;
+    let emailField2 = document.getElementById("email2").value;
+
+    if(emailField1 != emailField2) {
+      alert("Please make sure the e-mail fields match before submitting!");
+      return false;
+    }
+  }
