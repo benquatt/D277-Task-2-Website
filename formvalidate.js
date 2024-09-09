e89c0eace8ae659cb733f885e1e7256bdc72014e
benquatt <bquattl@wgu.edu>
2024-09-09 17:37:20 -0400

    Update formvalidate.js

--- formvalidate.js
+++ formvalidate.js
@@ -1,9 +1,8 @@
-function validateEmail() {
+  function validateEmail() {
     let emailField1 = document.getElementById("email1").value;
     let emailField2 = document.getElementById("email2").value;
 
-    if(emailField1 != emailField2) {
-      alert("Please make sure the e-mail fields match before submitting!");
-      return false;
+    if(emailField1 !== emailField2) {
+      alert("Please make sure the e-mail fields match!");
     }
   }
